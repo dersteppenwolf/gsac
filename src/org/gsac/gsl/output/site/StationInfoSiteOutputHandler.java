ddb749d39e45eeb5b17c4480a7714233a4621572
wier-unavco <wier-unavco@unavco.org>
2012-11-16 20:47:06 +0000

    initial version; does not yet do gamit station.info format
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@445 60a5d509-8669-43c4-ab2f-0b528a4e2583

---
+++ src/org/gsac/gsl/output/site/StationInfoSiteOutputHandler.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright 2010 UNAVCO, 6350 Nautilus Drive, Boulder, CO 80301
+ * http://www.unavco.org
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or (at
+ * your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ */
+
+package org.gsac.gsl.output.site;
+
+
+import org.gsac.gsl.*;
+import org.gsac.gsl.model.*;
+import org.gsac.gsl.output.*;
+
+
+import ucar.unidata.util.StringUtil;
+
+import java.io.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+/**
+ * Class specifies output format stationinfo, the GAMIT station.info format, and names the internal GSAC Java items OUTPUT_SITE_STATIONINFO, "site.stationinfo"
+ * This version has csv formatting code in handleresult () which needs to be rewritten to write GAMIT station.info format.
+ *
+ * @version     Nov 16, 2012 (initial) 
+ * @author      SKW
+ */
+public class StationInfoSiteOutputHandler extends GsacOutputHandler {
+
+    /** output id */
+    public static final String OUTPUT_SITE_STATIONINFO = "site.stationinfo";
+
+
+    /**
+     * _more_
+     *
+     * @param gsacRepository _more_
+     * @param resourceClass _more_
+     */
+    public StationInfoSiteOutputHandler(GsacRepository gsacRepository,
+                                 ResourceClass resourceClass) {
+        super(gsacRepository, resourceClass);
+        getRepository().addOutput(getResourceClass(),
+                                  new GsacOutput(this, OUTPUT_SITE_STATIONINFO,
+                                      "Site station.info", "/sites.stationinfo", true));
+                                    //  "Site station.info" is apparently a label for Results choice box in site search form on web page.
+                                    // the sites.stationinfo is used for names of files of results.   
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param request The request
+     * @param response The response
+     *
+     * @throws IOException On badness
+     * @throws ServletException On badness
+     */
+    public void handleResult(GsacRequest request, GsacResponse response)
+            throws IOException, ServletException {
+        response.startResponse(GsacResponse.MIME_CSV);
+        PrintWriter  pw          = response.getPrintWriter();
+        String       delimiter   = (request.defined(ARG_DELIMITER)
+                                    ? request.get(ARG_DELIMITER, " ")
+                                    : ",");
+        String       paramString = request.get(ARG_PARAMS, (String) null);
+
+        List<String> params      = ((paramString == null)
+                                    ? new ArrayList<String>()
+                                    : StringUtil.split(paramString, ",", true,
+                                        true));
+        if (params.size() == 0) {
+            params.add(ARG_SITE_ID);
+            params.add(ARG_SITE_CODE);
+            params.add(ARG_SITE_NAME);
+            params.add(ARG_SITE_LATITUDE);
+            params.add(ARG_SITE_LONGITUDE);
+            params.add(ARG_SITE_ELEVATION);
+        }
+        try {
+            int colCnt = 0;
+            for (String param : params) {
+                if (colCnt > 0) {
+                    pw.print(",");
+                } else {
+                    pw.print("#");
+                }
+                colCnt++;
+                pw.print(param);
+            }
+            pw.print("\n");
+
+            //            pw.print("#repositoryid, site code, name, latitude, longitude, elevation\n");
+            int siteCnt = 0;
+            for (GsacSite site : response.getSites()) {
+                siteCnt++;
+                colCnt = 0;
+                for (String param : params) {
+                    if (colCnt > 0) {
+                        pw.print(delimiter);
+                    }
+                    colCnt++;
+                    if (param.equals(ARG_SITE_CODE)) {
+                        pw.print(cleanString(site.getShortName(), delimiter));
+                    } else if (param.equals(ARG_SITE_NAME)) {
+                        pw.print(cleanString(site.getLongName(), delimiter));
+                    } else if (param.equals(ARG_SITE_ID)) {
+                        String id = site.getId();
+                        if (getRepository().isRemoteResource(site)) {
+                            String[] pair =
+                                getRepository().decodeRemoteId(id);
+                            id = pair[0] + ":" + pair[1];
+                        }
+                        pw.print(cleanString(id, delimiter));
+                    } else if (param.equals(ARG_SITE_LATITUDE)) {
+                        pw.print(site.getLatitude());
+                    } else if (param.equals(ARG_SITE_LONGITUDE)) {
+                        pw.print(site.getLongitude());
+                    } else if (param.equals(ARG_SITE_ELEVATION)) {
+                        pw.print(site.getElevation());
+                    } else if (param.equals(ARG_SITE_LOCATION)) {
+                        pw.print(site.getLatitude());
+                        pw.print(delimiter);
+                        pw.print(site.getLongitude());
+                        pw.print(delimiter);
+                        pw.print(site.getElevation());
+                    } else if (param.equals(ARG_SITE_TYPE)) {
+                        pw.print(site.getType().getId());
+                    } else {
+                        throw new IllegalArgumentException(
+                            "Unknown parameter:" + param);
+                    }
+                }
+                pw.print("\n");
+            }
+        } finally {
+            response.endResponse();
+        }
+    }
+
+
+    /**
+     * _more_
+     *
+     * @param s _more_
+     * @param delimiter _more_
+     *
+     * @return _more_
+     */
+    private String cleanString(String s, String delimiter) {
+        s = s.replaceAll(",", "_COMMA_");
+        s = s.replaceAll(delimiter, "\\" + delimiter);
+
+        return s;
+    }
+
+
+}
