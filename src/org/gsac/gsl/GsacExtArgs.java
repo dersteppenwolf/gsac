5836b764f63ea762a215bd84cd6e176b01aba64c
wier-unavco <wier-unavco@unavco.org>
2012-12-12 17:02:44 +0000

    better var name for freq standard name
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@527 60a5d509-8669-43c4-ab2f-0b528a4e2583

--- src/org/gsac/gsl/GsacExtArgs.java
+++ src/org/gsac/gsl/GsacExtArgs.java
@@ -80,6 +80,6 @@ public interface GsacExtArgs {
     public static final String SITE_METADATA_INDIVIDUALCALIBRATION = "site.metadata.calibration";
     public static final String SITE_METADATA_CDPNUM  = "site.metadata.cdpnum";
     public static final String SITE_METADATA_NAMEAGENCY = "site.metadata.nameagency";
-    public static final String METADATA_FREQUENCYSTANDARD = "metadata.frequencystandard";
+    public static final String SITE_METADATA_FREQUENCYSTANDARD = "metadata.frequencystandard";
 
 }
