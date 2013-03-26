8f48555ef15935d6a5083b87a8d755d742102a07
wier-unavco <wier-unavco@unavco.org>
2013-03-26 19:07:02 +0000

    RingFileManager.java: first step to initial working implementation
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@628 60a5d509-8669-43c4-ab2f-0b528a4e2583

--- src/org/ring/gsac/RingFileManager.java
+++ src/org/ring/gsac/RingFileManager.java
@@ -24,6 +24,9 @@ package org.ring.gsac;
 import org.gsac.gsl.*;
 import org.gsac.gsl.model.*;
 
+/* CHANGEME - done for INGV - include datahase package for the GSAC installation. */
+import org.ring.gsac.database.*;
+
 import ucar.unidata.sql.Clause;
 import ucar.unidata.sql.SqlUtil;
 import ucar.unidata.util.StringUtil;
@@ -35,6 +38,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
 
 /**
  * Handles all of the resource related repository requests. The main entry point is {@link #handleRequest}
@@ -43,6 +49,77 @@ import java.util.List;
  * @author  Jeff McWhirter
  */
 public class RingFileManager extends FileManager {
+
+    public static final String TYPE_GNSS_OBSERVATION = "gnss.observation";
+
+    private static ResourceType[] GNSS_FILE_TYPES = { new ResourceType(TYPE_GNSS_OBSERVATION, "GNSS - Observation") };
+
+    public static final String[] GNSS_METADATA_COLUMNS = new String[] {
+        Tables.SITI_GSAC.COL_ID_SITO,
+        Tables.SITI_GSAC.COL_NOME_SITO,
+        Tables.SITI_GSAC.COL_LUOGO,
+        Tables.SITI_GSAC.COL_LATITUDINE,
+        Tables.SITI_GSAC.COL_LONGITUDINE,
+        Tables.SITI_GSAC.COL_IERS_DOMES_NUMBER,
+        Tables.SITI_GSAC.COL_ID_RESPONSIBLE_AGENCY,
+        Tables.SITI_GSAC.COL_ID_MONUMENTO,
+        Tables.SITI_GSAC.COL_NAZIONE,
+        Tables.SITI_GSAC.COL_REGIONE,
+        Tables.SITI_GSAC.COL_AGENZIA,
+    };
+
+     /*
+public static class SITI_GSAC extends Tables {
+        public static final String NAME = "siti_gsac";
+
+        public String getName() {return NAME;}
+        public String getColumns() {return COLUMNS;}
+        public static final String COL_ID_SITO =  NAME + ".id_sito";
+        public static final String COL_NOME_SITO =  NAME + ".nome_sito";
+        public static final String COL_ID_RETE =  NAME + ".id_rete";
+        public static final String COL_DATA_DISMISSIONE =  NAME + ".data_dismissione";
+        public static final String COL_GEOLOGIA =  NAME + ".geologia";
+        public static final String COL_LUOGO =  NAME + ".luogo";
+        public static final String COL_LATITUDINE =  NAME + ".latitudine";
+        public static final String COL_LONGITUDINE =  NAME + ".longitudine";
+        public static final String COL_QUOTA =  NAME + ".quota";
+        public static final String COL_X =  NAME + ".x";
+        public static final String COL_Y =  NAME + ".y";
+        public static final String COL_Z =  NAME + ".z";
+        public static final String COL_X_UTMED50 =  NAME + ".x_utmed50";
+        public static final String COL_Y_UTMED50 =  NAME + ".y_utmed50";
+        public static final String COL_TIPO_MATERIALIZZAZIONE =  NAME + ".tipo_materializzazione";
+        public static final String COL_HEIGHT_OF_MONUMENT =  NAME + ".height_of_monument";
+        public static final String COL_MONUMENT_FOUNDATION =  NAME + ".monument_foundation";
+        public static final String COL_FOUNDATION_DEPTH =  NAME + ".foundation_depth";
+        public static final String COL_MONUMENT_INSCRIPTION =  NAME + ".monument_inscription";
+        public static final String COL_IERS_DOMES_NUMBER =  NAME + ".iers_domes_number";
+        public static final String COL_BEDROCK_TYPES =  NAME + ".bedrock_types";
+        public static final String COL_ID_ON_SITE_AGENCY =  NAME + ".id_on_site_agency";
+        public static final String COL_ID_RESPONSIBLE_AGENCY =  NAME + ".id_responsible_agency";
+        public static final String COL_ID_MONUMENTO =  NAME + ".id_monumento";
+        public static final String COL_NAZIONE =  NAME + ".nazione";
+        public static final String COL_REGIONE =  NAME + ".regione";
+        public static final String COL_ATTIVO =  NAME + ".attivo";
+        public static final String COL_NOTE =  NAME + ".note";
+        public static final String COL_ID_DATA_CENTER =  NAME + ".id_data_center";
+        public static final String COL_MONUMENTAZIONE_ESTERNA =  NAME + ".monumentazione_esterna";
+        public static final String COL_AGENZIA =  NAME + ".agenzia";
+        */
+
+    private static ResourceType[][] ALL_FILE_TYPES = { GNSS_FILE_TYPES };
+
+    /** _more_ */
+    public static final TimeZone TIMEZONE_UTC = TimeZone.getTimeZone("UTC");
+
+    /** _more_ */
+    private SimpleDateFormat sdf;
+
+    /** _more_ */
+    private SimpleDateFormat yyyyMMDDSdf;
+
+
+
 
     /**
      * ctor
@@ -71,6 +148,13 @@ public class RingFileManager extends FileManager {
           Arrays.sort(values);
           capabilities.add(new Capability("fruit", "Fruit Label", values, true));
         */
+
+        // look - which are?
+        addDefaultCapabilities(capabilities);
+
+        // from cddis: look
+        capabilities.addAll(getSiteManager().doGetQueryCapabilities());
+
         return capabilities;
     }
 
