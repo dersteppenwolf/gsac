96206a8624b9980d2eb1d8583a9d8dc915b00401
wier-unavco <wier-unavco@unavco.org>
2014-02-21 17:29:07 +0000

    change web page symbol for serial numbers from # to SN; GnssEquipmentGroup.java
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@911 60a5d509-8669-43c4-ab2f-0b528a4e2583

--- src/org/gsac/gsl/metadata/gnss/GnssEquipmentGroup.java
+++ src/org/gsac/gsl/metadata/gnss/GnssEquipmentGroup.java
@@ -191,7 +191,7 @@ public class GnssEquipmentGroup extends MetadataGroup {
         else if(name.length() == 0) name =  "-";
         buff.append(name);
         if (serial != null&& serial.length()>0) {
-            buff.append("<br><i>#" + serial + "</i>");
+            buff.append("<br><i>SN" + serial + "</i>");
         }
         buff.append("&nbsp;</td>");
 
