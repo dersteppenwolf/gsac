d780607acba99e2f3ec915952ba67032ffa3c132
wier-unavco <wier-unavco@unavco.org>
2014-04-21 17:47:45 +0000

    add more to XmlSiteLog.java for XmlSiteOutputHandler.java
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@976 60a5d509-8669-43c4-ab2f-0b528a4e2583

--- src/org/gsac/gsl/util/XmlSiteLog.java
+++ src/org/gsac/gsl/util/XmlSiteLog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 UNAVCO, 6350 Nautilus Drive, Boulder, CO 80301
+ * Copyright 2010, 2014 UNAVCO, 6350 Nautilus Drive, Boulder, CO 80301
  * http://www.unavco.org
  *
  * This library is free software; you can redistribute it and/or modify it
@@ -25,16 +25,18 @@ package org.gsac.gsl.util;
  * Class description
  *
  *
- * @version        Enter version here..., Wed, May 19, '10
- * @author         Enter your name here...
+ * @version        Wed. May 19, '10, for XML site log format.
+ * @author         J McWhirter
+ * @version        21 Apr 2104.  more items added for improved GSAC XML site log format
+ * @author         S K Wier
  */
 
 public class XmlSiteLog {
 
-
-
     /** _more_ */
     public static final String ATTR_XMLNS_XMLNS = "xmlns";
+    /** _more_ */
+    public static final String XMLNS_XMLNS = "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/2011";
 
     /** _more_ */
     public static final String ATTR_XMLNS_REALTIME = "xmlns:realtime";
@@ -56,117 +58,110 @@ public class XmlSiteLog {
 
     /** _more_ */
     public static final String ATTR_XSI_SCHEMALOCATION = "xsi:schemaLocation";
-
+    public static final String ATTR_GSAC_SCHEMALOCATION = "schemaLocation";
     /** _more_ */
     public static final String VALUE_XSI_SCHEMALOCATION =
         "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/2011  http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/2011/igsSiteLog.xsd";
-
-
-    /** _more_ */
-    public static final String XMLNS_XMLNS =
-        "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/2011";
+    public static final String VALUE_GSAC_SCHEMALOCATION = "";
 
     /** _more_ */
-    public static final String XMLNS_XMLNS_CONTACT =
-        "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/contact/2004";
+    public static final String XMLNS_XMLNS_CONTACT = "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/contact/2004";
 
     /** _more_ */
-    public static final String XMLNS_XMLNS_EQUIP =
-        "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/equipment/2004";
+    public static final String XMLNS_XMLNS_EQUIP = "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/equipment/2004";
 
     /** _more_ */
-    public static final String XMLNS_XMLNS_LI =
-        "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/localInterferences/2004";
+    public static final String XMLNS_XMLNS_LI = "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/localInterferences/2004";
 
     /** _more_ */
-    public static final String XMLNS_XMLNS_MI =
-        "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/monumentInfo/2004";
+    public static final String XMLNS_XMLNS_MI = "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/monumentInfo/2004";
 
     /** _more_ */
     public static final String XMLNS_XMLNS_REALTIME =
         "http://sopac.ucsd.edu/ns/geodesy/doc/igsSiteLog/realtimeDataInfo/2011";
 
     /** _more_ */
-    public static final String XMLNS_XMLNS_XSI =
-        "http://www.w3.org/2001/XMLSchema-instance";
+    public static final String XMLNS_XMLNS_XSI = "http://www.w3.org/2001/XMLSchema-instance";
 
     /** _more_ */
     public static final String TAG_IGSSITELOG = "igsSiteLog";
+    public static final String TAG_GSACXML = "GsacXmlSiteLog";
 
     /** _more_ */
     public static final String TAG_FORMINFORMATION = "formInformation";
 
     /** _more_ */
     public static final String TAG_MI_PREPAREDBY = "mi:preparedBy";
+    public static final String TAG_PREPAREDBY = "preparedBy";
 
     /** _more_ */
     public static final String TAG_MI_DATEPREPARED = "mi:datePrepared";
+    public static final String TAG_DATEPREPARED = "datePrepared";
 
     /** _more_ */
     public static final String TAG_MI_REPORTTYPE = "mi:reportType";
+    public static final String TAG_REPORTTYPE = "reportType";
 
     /** _more_ */
     public static final String TAG_SITEIDENTIFICATION = "siteIdentification";
 
     /** _more_ */
     public static final String TAG_MI_SITENAME = "mi:siteName";
+    public static final String TAG_SITENAME = "siteName";
 
     /** _more_ */
     public static final String TAG_MI_FOURCHARACTERID = "mi:fourCharacterID";
+    public static final String TAG_FOURCHARACTERID = "fourCharacterID";
 
     /** _more_ */
-    public static final String TAG_MI_MONUMENTINSCRIPTION =
-        "mi:monumentInscription";
+    public static final String TAG_MI_MONUMENTINSCRIPTION = "mi:monumentInscription";
+    public static final String TAG_MONUMENTINSCRIPTION = "monumentInscription";
 
     /** _more_ */
     public static final String TAG_MI_IERSDOMESNUMBER = "mi:iersDOMESNumber";
+    public static final String TAG_IERSDOMESNUMBER = "iersDOMESNumber";
 
     /** _more_ */
     public static final String TAG_MI_CDPNUMBER = "mi:cdpNumber";
+    public static final String TAG_CDPNUMBER = "cdpNumber";
 
     /** _more_ */
-    public static final String TAG_MI_MONUMENTDESCRIPTION =
-        "mi:monumentDescription";
+    public static final String TAG_MI_MONUMENTDESCRIPTION = "mi:monumentDescription";
+    public static final String TAG_MONUMENTDESCRIPTION = "monumentDescription";
 
     /** _more_ */
-    public static final String TAG_MI_HEIGHTOFTHEMONUMENT =
-        "mi:heightOfTheMonument";
+    public static final String TAG_MI_HEIGHTOFTHEMONUMENT = "mi:heightOfTheMonument";
 
     /** _more_ */
-    public static final String TAG_MI_MONUMENTFOUNDATION =
-        "mi:monumentFoundation";
+    public static final String TAG_MI_MONUMENTFOUNDATION = "mi:monumentFoundation";
 
     /** _more_ */
     public static final String TAG_MI_FOUNDATIONDEPTH = "mi:foundationDepth";
 
     /** _more_ */
-    public static final String TAG_MI_MARKERDESCRIPTION =
-        "mi:markerDescription";
+    public static final String TAG_MI_MARKERDESCRIPTION = "mi:markerDescription";
 
     /** _more_ */
     public static final String TAG_MI_DATEINSTALLED = "mi:dateInstalled";
+    public static final String TAG_DATEINSTALLED = "dateInstalled";
 
     /** _more_ */
-    public static final String TAG_MI_GEOLOGICCHARACTERISTIC =
-        "mi:geologicCharacteristic";
+    public static final String TAG_MI_GEOLOGICCHARACTERISTIC = "mi:geologicCharacteristic";
 
     /** _more_ */
     public static final String TAG_MI_BEDROCKTYPE = "mi:bedrockType";
 
     /** _more_ */
-    public static final String TAG_MI_BEDROCKCONDITION =
-        "mi:bedrockCondition";
+    public static final String TAG_MI_BEDROCKCONDITION = "mi:bedrockCondition";
 
     /** _more_ */
     public static final String TAG_MI_FRACTURESPACING = "mi:fractureSpacing";
 
     /** _more_ */
-    public static final String TAG_MI_FAULTZONESNEARBY =
-        "mi:faultZonesNearby";
+    public static final String TAG_MI_FAULTZONESNEARBY = "mi:faultZonesNearby";
 
     /** _more_ */
-    public static final String TAG_MI_DISTANCE_ACTIVITY =
-        "mi:distance-Activity";
+    public static final String TAG_MI_DISTANCE_ACTIVITY = "mi:distance-Activity";
 
     /** _more_ */
     public static final String TAG_MI_NOTES = "mi:notes";
@@ -176,41 +171,46 @@ public class XmlSiteLog {
 
     /** _more_ */
     public static final String TAG_MI_CITY = "mi:city";
+    public static final String TAG_CITY = "place_city";
 
     /** _more_ */
     public static final String TAG_MI_STATE = "mi:state";
+    public static final String TAG_STATE = "province_region_state";
 
     /** _more_ */
     public static final String TAG_MI_COUNTRY = "mi:country";
+    public static final String TAG_COUNTRY = "country";
 
     /** _more_ */
     public static final String TAG_MI_TECTONICPLATE = "mi:tectonicPlate";
 
     /** _more_ */
-    public static final String TAG_MI_APPROXIMATEPOSITIONITRF =
-        "mi:approximatePositionITRF";
+    public static final String TAG_MI_APPROXIMATEPOSITIONITRF = "mi:approximatePositionITRF";
+    public static final String TAG_APPROXIMATEPOSITIONITRF = "approximatePositionITRF";
 
     /** _more_ */
-    public static final String TAG_MI_XCOORDINATEINMETERS =
-        "mi:xCoordinateInMeters";
+    public static final String TAG_MI_XCOORDINATEINMETERS = "mi:xCoordinateInMeters";
+    public static final String TAG_XCOORDINATEINMETERS = "xCoordinateInMeters";
 
     /** _more_ */
-    public static final String TAG_MI_YCOORDINATEINMETERS =
-        "mi:yCoordinateInMeters";
+    public static final String TAG_MI_YCOORDINATEINMETERS = "mi:yCoordinateInMeters";
+    public static final String TAG_YCOORDINATEINMETERS = "yCoordinateInMeters";
 
     /** _more_ */
-    public static final String TAG_MI_ZCOORDINATEINMETERS =
-        "mi:zCoordinateInMeters";
+    public static final String TAG_MI_ZCOORDINATEINMETERS = "mi:zCoordinateInMeters";
+    public static final String TAG_ZCOORDINATEINMETERS = "zCoordinateInMeters";
 
     /** _more_ */
     public static final String TAG_MI_LATITUDE_NORTH = "mi:latitude-North";
+    public static final String TAG_LATITUDE_NORTH = "latitude-North";
 
     /** _more_ */
     public static final String TAG_MI_LONGITUDE_EAST = "mi:longitude-East";
+    public static final String TAG_LONGITUDE_EAST = "longitude-East";
 
     /** _more_ */
-    public static final String TAG_MI_ELEVATION_M_ELLIPS =
-        "mi:elevation-m_ellips";
+    public static final String TAG_MI_ELEVATION_M_ELLIPS = "mi:elevation-m_ellips";
+    public static final String TAG_ELLIPS_HGT = "ellipsoid_hgt";
 
     /** _more_ */
     public static final String TAG_GNSSRECEIVER = "gnssReceiver";
@@ -219,30 +219,25 @@ public class XmlSiteLog {
     public static final String TAG_EQUIP_RECEIVERTYPE = "equip:receiverType";
 
     /** _more_ */
-    public static final String TAG_EQUIP_SATELLITESYSTEM =
-        "equip:satelliteSystem";
+    public static final String TAG_EQUIP_SATELLITESYSTEM = "equip:satelliteSystem";
 
     /** _more_ */
     public static final String TAG_EQUIP_SERIALNUMBER = "equip:serialNumber";
 
     /** _more_ */
-    public static final String TAG_EQUIP_FIRMWAREVERSION =
-        "equip:firmwareVersion";
+    public static final String TAG_EQUIP_FIRMWAREVERSION = "equip:firmwareVersion";
 
     /** _more_ */
-    public static final String TAG_EQUIP_ELEVATIONCUTOFFSETTING =
-        "equip:elevationCutoffSetting";
+    public static final String TAG_EQUIP_ELEVATIONCUTOFFSETTING = "equip:elevationCutoffSetting";
 
     /** _more_ */
-    public static final String TAG_EQUIP_DATEINSTALLED =
-        "equip:dateInstalled";
+    public static final String TAG_EQUIP_DATEINSTALLED = "equip:dateInstalled";
 
     /** _more_ */
     public static final String TAG_EQUIP_DATEREMOVED = "equip:dateRemoved";
 
     /** _more_ */
-    public static final String TAG_EQUIP_TEMPERATURESTABILIZATION =
-        "equip:temperatureStabilization";
+    public static final String TAG_EQUIP_TEMPERATURESTABILIZATION = "equip:temperatureStabilization";
 
     /** _more_ */
     public static final String TAG_EQUIP_NOTES = "equip:notes";
@@ -254,47 +249,47 @@ public class XmlSiteLog {
     public static final String TAG_EQUIP_ANTENNATYPE = "equip:antennaType";
 
     /** _more_ */
-    public static final String TAG_EQUIP_ANTENNAREFERENCEPOINT =
-        "equip:antennaReferencePoint";
+    public static final String TAG_EQUIP_ANTENNAREFERENCEPOINT = "equip:antennaReferencePoint";
 
     /** _more_ */
-    public static final String TAG_EQUIP_MARKER_ARPUPECC =
-        "equip:marker-arpUpEcc.";
+    public static final String TAG_EQUIP_MARKER_ARPUPECC = 
+        "equip:marker-arpUpEcc";
 
     /** _more_ */
     public static final String TAG_EQUIP_MARKER_ARPNORTHECC =
-        "equip:marker-arpNorthEcc.";
+        "equip:marker-arpNorthEcc";
 
     /** _more_ */
     public static final String TAG_EQUIP_MARKER_ARPEASTECC =
-        "equip:marker-arpEastEcc.";
+        "equip:marker-arpEastEcc";
 
     /** _more_ */
-    public static final String TAG_EQUIP_ALIGNMENTFROMTRUENORTH =
+    public static final String TAG_EQUIP_ALIGNMENTFROMTRUENORTH = 
         "equip:alignmentFromTrueNorth";
 
     /** _more_ */
-    public static final String TAG_EQUIP_ANTENNARADOMETYPE =
-        "equip:antennaRadomeType";
+    public static final String TAG_EQUIP_ANTENNARADOMETYPE = 
+      "equip:antennaRadomeType";
 
     /** _more_ */
-    public static final String TAG_EQUIP_RADOMESERIALNUMBER =
-        "equip:radomeSerialNumber";
+    public static final String TAG_EQUIP_RADOMESERIALNUMBER = 
+           "equip:radomeSerialNumber";
 
     /** _more_ */
-    public static final String TAG_EQUIP_ANTENNACABLETYPE =
-        "equip:antennaCableType";
+    public static final String TAG_EQUIP_ANTENNACABLETYPE = 
+         "equip:antennaCableType";
 
     /** _more_ */
-    public static final String TAG_EQUIP_ANTENNACABLELENGTH =
-        "equip:antennaCableLength";
+    public static final String TAG_EQUIP_ANTENNACABLELENGTH = 
+         "equip:antennaCableLength";
 
     /** _more_ */
-    public static final String TAG_FREQUENCYSTANDARD = "frequencyStandard";
+    public static final String TAG_FREQUENCYSTANDARD = 
+          "frequencyStandard";
 
     /** _more_ */
-    public static final String TAG_COLOCATIONINFORMATION =
-        "colocationInformation";
+    public static final String TAG_COLOCATIONINFORMATION = 
+          "colocationInformation";
 
     /** _more_ */
     public static final String TAG_HUMIDITYSENSOR = "humiditySensor";
@@ -306,8 +301,7 @@ public class XmlSiteLog {
     public static final String TAG_WATERVAPORSENSOR = "waterVaporSensor";
 
     /** _more_ */
-    public static final String TAG_OTHERINSTRUMENTATION =
-        "otherInstrumentation";
+    public static final String TAG_OTHERINSTRUMENTATION = "otherInstrumentation";
 
     /** _more_ */
     public static final String TAG_RADIOINTERFERENCES = "radioInterferences";
@@ -319,8 +313,7 @@ public class XmlSiteLog {
     public static final String TAG_SIGNALOBSTRUCTIONS = "signalObstructions";
 
     /** _more_ */
-    public static final String TAG_LOCALEPISODICEVENTS =
-        "localEpisodicEvents";
+    public static final String TAG_LOCALEPISODICEVENTS = "localEpisodicEvents";
 
     /** _more_ */
     public static final String TAG_CONTACTAGENCY = "contactAgency";
@@ -329,21 +322,16 @@ public class XmlSiteLog {
     public static final String TAG_RESPONSIBLEAGENCY = "responsibleAgency";
 
     /** _more_ */
-    public static final String TAG_REALTIMEDATASTREAMS =
-        "realtimeDataStreams";
+    public static final String TAG_REALTIMEDATASTREAMS = "realtimeDataStreams";
 
     /** _more_ */
     public static final String TAG_MOREINFORMATION = "moreInformation";
 
-
+    /** _more_ */
+    public static final String TAG_REALTIME_DATASTREAMS = "realtime:dataStreams";
 
     /** _more_ */
-    public static final String TAG_REALTIME_DATASTREAMS =
-        "realtime:dataStreams";
-
-    /** _more_ */
-    public static final String TAG_REALTIME_SITESTREAM =
-        "realtime:siteStream";
+    public static final String TAG_REALTIME_SITESTREAM = "realtime:siteStream";
 
     /** _more_ */
     public static final String TAG_REALTIME_HOSTNAME = "realtime:hostname";
@@ -358,46 +346,37 @@ public class XmlSiteLog {
     public static final String TAG_REALTIME_SOLUTION = "realtime:solution";
 
     /** _more_ */
-    public static final String TAG_REALTIME_SAMPINTERVAL =
-        "realtime:sampInterval";
+    public static final String TAG_REALTIME_SAMPINTERVAL = "realtime:sampInterval";
 
     /** _more_ */
-    public static final String TAG_REALTIME_DATAFORMAT =
-        "realtime:dataFormat";
+    public static final String TAG_REALTIME_DATAFORMAT = "realtime:dataFormat";
 
     /** _more_ */
-    public static final String TAG_REALTIME_NTRIPPARAMS =
-        "realtime:ntripParams";
+    public static final String TAG_REALTIME_NTRIPPARAMS = "realtime:ntripParams";
 
     /** _more_ */
-    public static final String TAG_REALTIME_NTRIPMOUNT =
-        "realtime:ntripMount";
+    public static final String TAG_REALTIME_NTRIPMOUNT = "realtime:ntripMount";
 
     /** _more_ */
-    public static final String TAG_REALTIME_MOUNTPOINT =
-        "realtime:mountPoint";
+    public static final String TAG_REALTIME_MOUNTPOINT = "realtime:mountPoint";
 
     /** _more_ */
     public static final String TAG_REALTIME_SOURCEID = "realtime:sourceID";
 
     /** _more_ */
-    public static final String TAG_REALTIME_COUNTRYCODE =
-        "realtime:countryCode";
+    public static final String TAG_REALTIME_COUNTRYCODE = "realtime:countryCode";
 
     /** _more_ */
     public static final String TAG_REALTIME_NETWORK = "realtime:network";
 
     /** _more_ */
-    public static final String TAG_REALTIME_ALLOWCONNECTIONS =
-        "realtime:allowConnections";
+    public static final String TAG_REALTIME_ALLOWCONNECTIONS = "realtime:allowConnections";
 
     /** _more_ */
-    public static final String TAG_REALTIME_REQUIREAUTHENTICATION =
-        "realtime:requireAuthentication";
+    public static final String TAG_REALTIME_REQUIREAUTHENTICATION = "realtime:requireAuthentication";
 
     /** _more_ */
-    public static final String TAG_REALTIME_ENCRYPTION =
-        "realtime:encryption";
+    public static final String TAG_REALTIME_ENCRYPTION = "realtime:encryption";
 
     /** _more_ */
     public static final String TAG_REALTIME_FEESAPPLY = "realtime:feesApply";
@@ -406,20 +385,15 @@ public class XmlSiteLog {
     public static final String TAG_REALTIME_BITRATE = "realtime:bitrate";
 
     /** _more_ */
-    public static final String TAG_REALTIME_CARRIERPHASE =
-        "realtime:carrierPhase";
+    public static final String TAG_REALTIME_CARRIERPHASE = "realtime:carrierPhase";
 
     /** _more_ */
     public static final String TAG_REALTIME_NAVSYSTEM = "realtime:navSystem";
 
     /** _more_ */
-    public static final String TAG_REALTIME_PUBLISHEDSTREAM =
-        "realtime:publishedStream";
+    public static final String TAG_REALTIME_PUBLISHEDSTREAM = "realtime:publishedStream";
 
     /** _more_ */
     public static final String TAG_REALTIME_IPADDRESS = "realtime:ipAddress";
-
-
-
-
 }
+
