b5137e685f094f6d75203430ad9be7c5ecb94713
wier-unavco <wier-unavco@unavco.org>
2014-09-04 19:52:19 +0000

    improved Dataworks database schema, as MySQL file
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@1045 60a5d509-8669-43c4-ab2f-0b528a4e2583

--- src/org/dataworks/gsac/Dataworks_GSAC_MySQL_schema.sql
+++ src/org/dataworks/gsac/Dataworks_GSAC_MySQL_schema.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: Dataworks_GSAC
 -- ------------------------------------------------------
--- Server version	5.1.69
+-- Server version	5.1.69-0ubuntu0.10.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -35,7 +35,7 @@ CREATE TABLE `access` (
   `access_description` varchar(80) NOT NULL,
   `embargo_duration_days` int(6) NOT NULL DEFAULT '0',
   PRIMARY KEY (`access_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,7 +44,7 @@ CREATE TABLE `access` (
 
 LOCK TABLES `access` WRITE;
 /*!40000 ALTER TABLE `access` DISABLE KEYS */;
-INSERT INTO `access` VALUES (1,'no public access allowed',0),(2,'public access allowed for station metadata, instrument metadata, and data files',0),(3,'public access allowed for station and  instrument metadata only',0);
+INSERT INTO `access` VALUES (1,'no public access allowed',0),(2,'public access allowed for station metadata, instrument metadata, and data files',0),(3,'public access allowed for station and         instrument metadata only',0);
 /*!40000 ALTER TABLE `access` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ CREATE TABLE `agency` (
   `agency_name` varchar(100) NOT NULL,
   `agency_short_name` varchar(10) NOT NULL,
   PRIMARY KEY (`agency_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,6 +69,7 @@ CREATE TABLE `agency` (
 
 LOCK TABLES `agency` WRITE;
 /*!40000 ALTER TABLE `agency` DISABLE KEYS */;
+INSERT INTO `agency` VALUES (1,'not specified',' ');
 /*!40000 ALTER TABLE `agency` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +85,7 @@ CREATE TABLE `antenna` (
   `antenna_name` varchar(15) NOT NULL,
   `igs_defined` char(1) NOT NULL DEFAULT 'N',
   PRIMARY KEY (`antenna_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,7 +108,7 @@ CREATE TABLE `country` (
   `country_id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `country_name` varchar(70) NOT NULL,
   PRIMARY KEY (`country_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -127,11 +128,11 @@ DROP TABLE IF EXISTS `datafile`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `datafile` (
-  `datafile_id` int(9) NOT NULL AUTO_INCREMENT,
-  `station_id` int(6) NOT NULL,
-  `equip_config_id` int(6) NOT NULL,
+  `datafile_id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `station_id` int(6) unsigned NOT NULL,
+  `equip_config_id` int(6) unsigned NOT NULL,
   `datafile_name` varchar(120) NOT NULL,
-  `datafile_format_id` int(3) NOT NULL,
+  `datafile_format_id` int(3) unsigned NOT NULL,
   `sample_interval` float NOT NULL,
   `datafile_start_time` datetime NOT NULL,
   `datafile_stop_time` datetime NOT NULL,
@@ -140,9 +141,15 @@ CREATE TABLE `datafile` (
   `published_time` datetime NOT NULL,
   `size_bytes` int(10) NOT NULL,
   `MD5` char(32) NOT NULL,
-  `url_path` varchar(120) NOT NULL,
-  PRIMARY KEY (`datafile_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `URL_path` varchar(120) NOT NULL,
+  PRIMARY KEY (`datafile_id`),
+  KEY `station_id_idx` (`station_id`),
+  KEY `equip_config_id_idx` (`equip_config_id`),
+  KEY `datafile_format_id_idx` (`datafile_format_id`),
+  CONSTRAINT `station_id2` FOREIGN KEY (`station_id`) REFERENCES `station` (`station_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `equip_config_id` FOREIGN KEY (`equip_config_id`) REFERENCES `equip_config` (`equip_config_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `datafile_format_id` FOREIGN KEY (`datafile_format_id`) REFERENCES `datafile_format` (`datafile_format_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -165,9 +172,9 @@ CREATE TABLE `datafile_format` (
   `datafile_format_id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `datafile_format_name` varchar(50) NOT NULL,
   `datafile_format_version` varchar(50) NOT NULL,
-  `datafile_format_description` varchar(100) DEFAULT NULL,
+  `datafile_format_description` varchar(50) NOT NULL,
   PRIMARY KEY (`datafile_format_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -176,7 +183,7 @@ CREATE TABLE `datafile_format` (
 
 LOCK TABLES `datafile_format` WRITE;
 /*!40000 ALTER TABLE `datafile_format` DISABLE KEYS */;
-INSERT INTO `datafile_format` VALUES (1,'instrument data file','','Any type or format of native, raw, or binary file from an instrument.'),(2,'RINEX observation file','','RINEX obs file of any type: o, d, .zip, d.zip, etc.'),(3,'RINEX GPS navigation file','',''),(4,'RINEX Galileo navigation file','',''),(5,'RINEX GLONASS navigation file','',''),(6,'RINEX meteorology file','','');
+INSERT INTO `datafile_format` VALUES (1,'instrument data file','','Any type or format of native, raw, or binary file '),(2,'RINEX observation file','','RINEX obs file of any type: o, d, .zip, d.zip, etc'),(3,'RINEX GPS navigation file','',''),(4,'RINEX Galileo navigation file','',''),(5,'RINEX GLONASS navigation file','',''),(6,'RINEX meteorology file','','');
 /*!40000 ALTER TABLE `datafile_format` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -192,7 +199,7 @@ CREATE TABLE `ellipsoid` (
   `ellipsoid_name` varchar(45) NOT NULL,
   `ellipsoid_short_name` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`ellipsoid_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -201,7 +208,7 @@ CREATE TABLE `ellipsoid` (
 
 LOCK TABLES `ellipsoid` WRITE;
 /*!40000 ALTER TABLE `ellipsoid` DISABLE KEYS */;
-INSERT INTO `ellipsoid` VALUES (1,'WGS 84','WGS 84'),(2,'GRS 80','GRS 80');
+INSERT INTO `ellipsoid` VALUES (1,'WGS 84','WGS 84'),(2,'GRS 80','GRS 80'),(3,'PZ-90','PZ-90');
 /*!40000 ALTER TABLE `ellipsoid` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -217,7 +224,7 @@ CREATE TABLE `equip_config` (
   `station_id` int(6) unsigned NOT NULL,
   `create_time` datetime NOT NULL,
   `equip_config_start_time` datetime NOT NULL,
-  `equip_config_stop_time` datetime NOT NULL,
+  `equip_config_stop_time` datetime DEFAULT NULL,
   `antenna_id` int(3) unsigned NOT NULL,
   `antenna_serial_number` varchar(20) NOT NULL,
   `antenna_height` float NOT NULL,
@@ -225,12 +232,22 @@ CREATE TABLE `equip_config` (
   `metpack_serial_number` varchar(20) DEFAULT NULL,
   `radome_id` int(3) unsigned NOT NULL,
   `radome_serial_number` varchar(20) NOT NULL,
-  `receiver_firmware_id` int(3) unsigned NOT NULL,
-  `receiver_firmware_vers` varchar(20) NOT NULL,
+  `receiver_id` int(3) unsigned NOT NULL,
   `receiver_serial_number` varchar(20) NOT NULL,
-  `satellite_system` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`equip_config_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `receiver_firmware_vers` varchar(20) NOT NULL,
+  `satellite_system` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`equip_config_id`),
+  KEY `station_id_idx` (`station_id`),
+  KEY `antenna_id_idx` (`antenna_id`),
+  KEY `metpack_id_idx` (`metpack_id`),
+  KEY `receiver_id_idx` (`receiver_id`),
+  KEY `radome_id_idx` (`radome_id`),
+  CONSTRAINT `antenna_id` FOREIGN KEY (`antenna_id`) REFERENCES `antenna` (`antenna_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `metpack_id` FOREIGN KEY (`metpack_id`) REFERENCES `metpack` (`metpack_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `radome_id` FOREIGN KEY (`radome_id`) REFERENCES `radome` (`radome_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `receiver_id` FOREIGN KEY (`receiver_id`) REFERENCES `receiver` (`receiver_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `station_id` FOREIGN KEY (`station_id`) REFERENCES `station` (`station_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -253,7 +270,7 @@ CREATE TABLE `locale` (
   `locale_id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `locale_info` varchar(70) NOT NULL,
   PRIMARY KEY (`locale_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -276,7 +293,7 @@ CREATE TABLE `metpack` (
   `metpack_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
   `metpack_name` varchar(15) NOT NULL,
   PRIMARY KEY (`metpack_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -285,7 +302,7 @@ CREATE TABLE `metpack` (
 
 LOCK TABLES `metpack` WRITE;
 /*!40000 ALTER TABLE `metpack` DISABLE KEYS */;
-INSERT INTO `metpack` VALUES (1,'no metpack');
+INSERT INTO `metpack` VALUES (1,'not specified'), (2,'no metpack');
 /*!40000 ALTER TABLE `metpack` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -300,7 +317,7 @@ CREATE TABLE `monument_style` (
   `monument_style_id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `monument_style_description` varchar(70) NOT NULL,
   PRIMARY KEY (`monument_style_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -309,7 +326,7 @@ CREATE TABLE `monument_style` (
 
 LOCK TABLES `monument_style` WRITE;
 /*!40000 ALTER TABLE `monument_style` DISABLE KEYS */;
-INSERT INTO `monument_style` VALUES (1,'INOX METAL PILLAR'),(2,'METAL PILLAR'),(3,'PILLAR'),(4,'STEEL MAST');
+INSERT INTO `monument_style` VALUES (1,'not specified');
 /*!40000 ALTER TABLE `monument_style` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -324,7 +341,7 @@ CREATE TABLE `network` (
   `network_id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `network_name` varchar(50) NOT NULL,
   PRIMARY KEY (`network_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -348,7 +365,7 @@ CREATE TABLE `radome` (
   `radome_name` varchar(15) NOT NULL,
   `igs_defined` char(1) NOT NULL DEFAULT 'N',
   PRIMARY KEY (`radome_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -357,6 +374,7 @@ CREATE TABLE `radome` (
 
 LOCK TABLES `radome` WRITE;
 /*!40000 ALTER TABLE `radome` DISABLE KEYS */;
+INSERT INTO `radome` VALUES (1,'not specified','N');
 /*!40000 ALTER TABLE `radome` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -368,11 +386,11 @@ DROP TABLE IF EXISTS `receiver`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `receiver` (
-  `receiver_firmware_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
+  `receiver_id` int(5) unsigned NOT NULL AUTO_INCREMENT,
   `receiver_name` varchar(20) NOT NULL,
   `igs_defined` char(1) NOT NULL DEFAULT 'N',
-  PRIMARY KEY (`receiver_firmware_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`receiver_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -381,6 +399,7 @@ CREATE TABLE `receiver` (
 
 LOCK TABLES `receiver` WRITE;
 /*!40000 ALTER TABLE `receiver` DISABLE KEYS */;
+INSERT INTO `receiver` VALUES (1,'not specified','N');
 /*!40000 ALTER TABLE `receiver` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -407,18 +426,34 @@ CREATE TABLE `station` (
   `country_id` int(3) unsigned NOT NULL,
   `locale_id` int(3) unsigned NOT NULL,
   `ellipsoid_id` int(1) unsigned NOT NULL,
-  `x` double DEFAULT NULL,
-  `y` double DEFAULT NULL,
-  `z` double DEFAULT NULL,
   `iers_domes` char(9) DEFAULT NULL,
-  `station_photo_URL` varchar(100) DEFAULT NULL,
-  `time_series_plot_photo_URL` varchar(100) DEFAULT NULL,
   `operator_agency_id` int(3) unsigned NOT NULL,
   `data_publisher_agency_id` int(3) unsigned NOT NULL,
   `network_id` int(5) unsigned NOT NULL,
-  `networks` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`station_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `station_photo_URL` varchar(100) DEFAULT NULL,
+  `time_series_plot_photo_URL` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`station_id`),
+  KEY `style_id_idx` (`style_id`),
+  KEY `status_id_idx` (`status_id`),
+  KEY `access_id_idx` (`access_id`),
+  KEY `monument_style_id_idx` (`monument_style_id`),
+  KEY `country_id_idx` (`country_id`),
+  KEY `locale_id_idx` (`locale_id`),
+  KEY `ellipsoid_id_idx` (`ellipsoid_id`),
+  KEY `operator_agency_id_idx` (`operator_agency_id`),
+  KEY `data_publisher_agency_id_idx` (`data_publisher_agency_id`),
+  KEY `network_id_idx` (`network_id`),
+  CONSTRAINT `access_id` FOREIGN KEY (`access_id`) REFERENCES `access` (`access_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `country_id` FOREIGN KEY (`country_id`) REFERENCES `country` (`country_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `data_publisher_agency_id` FOREIGN KEY (`data_publisher_agency_id`) REFERENCES `agency` (`agency_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `ellipsoid_id` FOREIGN KEY (`ellipsoid_id`) REFERENCES `ellipsoid` (`ellipsoid_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `locale_id` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`locale_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `monument_style_id` FOREIGN KEY (`monument_style_id`) REFERENCES `monument_style` (`monument_style_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `network_id` FOREIGN KEY (`network_id`) REFERENCES `network` (`network_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `operator_agency_id` FOREIGN KEY (`operator_agency_id`) REFERENCES `agency` (`agency_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `status_id` FOREIGN KEY (`status_id`) REFERENCES `station_status` (`station_status_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `style_id` FOREIGN KEY (`style_id`) REFERENCES `station_style` (`station_style_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -441,7 +476,7 @@ CREATE TABLE `station_status` (
   `station_status_id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `station_status` varchar(80) NOT NULL,
   PRIMARY KEY (`station_status_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -465,7 +500,7 @@ CREATE TABLE `station_style` (
   `station_style_id` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `station_style_description` varchar(80) NOT NULL,
   PRIMARY KEY (`station_style_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -474,7 +509,7 @@ CREATE TABLE `station_style` (
 
 LOCK TABLES `station_style` WRITE;
 /*!40000 ALTER TABLE `station_style` DISABLE KEYS */;
-INSERT INTO `station_style` VALUES (1,'GPS/GNSS Campaign'),(2,'GPS/GNSS Continuous'),(3,'GPS/GNSS Mobile');
+INSERT INTO `station_style` VALUES (1,'GPS/GNSS Continuous'),(2,'GPS/GNSS Campaign'),(3,'GPS/GNSS Mobile');
 /*!40000 ALTER TABLE `station_style` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -487,4 +522,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-08-13  9:02:40
+-- Dump completed on 2014-09-04 11:27:16
