8114b2626b6142e56a8b2571ff3a75ad3e6e02b7
wier-unavco <wier-unavco@unavco.org>
2015-01-29 18:57:33 +0000

    new GsacServer.java for Java 6 and Java 7 in gsc/gsl/
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@1179 60a5d509-8669-43c4-ab2f-0b528a4e2583

--- src/org/gsac/gsl/GsacServer.java
+++ src/org/gsac/gsl/GsacServer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 UNAVCO, 6350 Nautilus Drive, Boulder, CO 80301
+ * Copyright 2015 UNAVCO, 6350 Nautilus Drive, Boulder, CO 80301
  * http://www.unavco.org
  *
  * This library is free software; you can redistribute it and/or modify it
@@ -31,6 +31,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/* ************************************************************
+ *
+ * import block for Java 1.7 build of GSAC
+ * 
+ * Do not include these lines inside a Java comment 
+ * to build GSAC with Java 1.7
+ *
+ *************************************************************
+ */
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Handler;
@@ -38,10 +47,33 @@ import org.eclipse.jetty.server.NCSARequestLog;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.*;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
+
+/* ************************************************************
+ *
+ * import block for Java 1.6 build of GSAC
+ * 
+ * Do not include these lines inside a Java comment 
+ * to build GSAC with Java 1.6
+ *
+ *************************************************************
+
+import org.mortbay.jetty.*;
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Handler;
+import org.mortbay.jetty.HttpConnection;
+import org.mortbay.jetty.NCSARequestLog;
+import org.mortbay.jetty.Request;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.bio.SocketConnector;
+import org.mortbay.jetty.handler.*;
+import org.mortbay.jetty.handler.AbstractHandler;
+import org.mortbay.jetty.security.SslSocketConnector;
+import org.mortbay.jetty.servlet.Context;
+import org.mortbay.jetty.servlet.ServletHolder;
+*/
 
 
 /**
@@ -50,7 +82,8 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
  * the doMakeServlet factory method to create a servlet with their own implementation of
  * the GsacRepository.
  *
- * 23 Dec 2014: updated to javax servlet-api 3.0 and jetty 9.
+ * 29 Jan 2015: merged version for either Java 1.6 or Java 1.7 GSAC builds.
+ * Look for lines with " Java 1.6 " and " Java 1.7 " and use accordingly.
  *
  */
 public class GsacServer {
@@ -86,12 +119,52 @@ public class GsacServer {
             }
         }
 
+
+        /* ************************************************************
+         *
+         * code block for Java 1.6 build of GSAC
+         * 
+         * Do not include these lines inside a Java comment 
+         * to build GSAC with Java 1.6
+         *
+         *************************************************************
+         
         GsacServlet              gsacServlet = doMakeServlet(port, properties);
+        Server                   server      = new Server(port);
+        HandlerCollection        handlers    = new HandlerCollection();
+        ContextHandlerCollection contexts    = new ContextHandlerCollection();
+        Context context = new Context(contexts, "/", Context.SESSIONS);
+        context.addServlet(new ServletHolder(gsacServlet), gsacServlet.getRepository().getUrlBase() + "/*");
+        handlers.setHandlers(new Handler[] { contexts, new DefaultHandler() });
+        System.out.println( "Running stand-alone GSAC server at: http://localhost:" + port + gsacServlet.getRepository().getUrlBase());
+        server.setHandler(handlers);
+        */
 
+
+        /* ************************************************************
+         *
+         * code block for Java 1.7 build of GSAC
+         * 
+         * Do not include these lines inside a Java comment 
+         * to build GSAC with Java 1.7
+         *
+         *************************************************************
+         */
+        GsacServlet gsacServlet = doMakeServlet(port, properties);
         Server server = new Server(port);  
-
         ServletContextHandler contexthandler = new ServletContextHandler(ServletContextHandler.SESSIONS); 
         contexthandler.setContextPath("/"); // technically not required, as "/" is the default
+        ServletHolder holderPwd = new ServletHolder("default", gsacServlet);
+        contexthandler.addServlet(holderPwd, "/*");
+        System.out.println( "Running stand-alone GSAC server at: http://localhost:" + port + gsacServlet.getRepository().getUrlBase());
+        server.setHandler(contexthandler); 
+
+
+        server.start(); 
+        server.join(); 
+
+    }
+ 
 
         ServletHolder holderPwd = new ServletHolder("default", gsacServlet);
 
