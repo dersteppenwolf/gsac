46313637d16f6f7b873541272e8012ae321e8ef3
wier-unavco <wier-unavco@unavco.org>
2015-02-18 20:11:50 +0000

    correct template/Server.java
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@1226 60a5d509-8669-43c4-ab2f-0b528a4e2583

--- src/org/gsac/template/Server.java
+++ src/org/gsac/template/Server.java
@@ -71,36 +71,6 @@ import org.mortbay.jetty.servlet.ServletHolder;
 */
 
 
-///
-
-import org.gsac.gsl.*;
-
-import org.mortbay.jetty.*;
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Handler;
-import org.mortbay.jetty.HttpConnection;
-import org.mortbay.jetty.NCSARequestLog;
-import org.mortbay.jetty.Request;
-import org.mortbay.jetty.Server;
-
-
-import org.mortbay.jetty.bio.SocketConnector;
-import org.mortbay.jetty.handler.*;
-import org.mortbay.jetty.handler.AbstractHandler;
-
-import org.mortbay.jetty.security.SslSocketConnector;
-import org.mortbay.jetty.servlet.Context;
-import org.mortbay.jetty.servlet.ServletHolder;
-
-
-
-import java.io.*;
-import java.util.Properties;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-
-
 
 
 /**
