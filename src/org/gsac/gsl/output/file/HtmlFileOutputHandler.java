504302824d5b1e170b25da23ad49b8943d2c3b94
wier-unavco <wier-unavco@unavco.org>
2015-03-26 17:26:54 +0000

    improve table col headers in HtmlFileOutputHandler.java
    git-svn-id: https://svn.code.sf.net/p/gsac/code/trunk@1300 60a5d509-8669-43c4-ab2f-0b528a4e2583

--- src/org/gsac/gsl/output/file/HtmlFileOutputHandler.java
+++ src/org/gsac/gsl/output/file/HtmlFileOutputHandler.java
@@ -369,8 +369,9 @@ public class HtmlFileOutputHandler extends HtmlOutputHandler {
                     }
                     else {  
                        // note that the HTML term for the Greek letter delta is &Delta;
+                       //labels = new String[] {  msg("File URL to download"), msg("File type"),msg("Time range of data"), msg("&Delta;t"), msg("MD5 check sum"), msg("File size") };
                        // Standard, and for for prototype GSAC:
-                       labels = new String[] {  msg("File URL to download"), msg("File type"),msg("Time range of data"), msg("&Delta;t"), msg("MD5 check sum"), msg("File size") };
+                       labels = new String[] {  msg("File URL to download"), msg("File type"),msg("Time range of data"), msg("MD5 check sum"), msg("File size") };
                        // CHANGEME for UNR:
                        //labels = new String[] {  msg("File URL to download"), msg("File type"),msg("Time range of data") };
                     }
